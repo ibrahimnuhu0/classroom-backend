18042ea1de3b085a24cf718f6df6d8e8dbad9dec
Ibrahim Nuhu <ibrahimnuhu0@gmail.com>
2026-02-25 14:47:38 +0100

    add auth extension

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { index } from 'drizzle-orm/pg-core';
 import AgentAPI from 'apminsight';
 AgentAPI.config()
 
@@ -8,7 +9,7 @@ import cors from 'cors'
 import subjectsRouter from "./routes/subjects.js"
 import securityMiddleware from './middleware/security.js';
 import {toNodeHandler} from  "better-auth/node";
-import { auth } from './lib/auth';
+import { auth } from './lib/auth.js';
 
 const app = express();
 const PORT = 8000;
